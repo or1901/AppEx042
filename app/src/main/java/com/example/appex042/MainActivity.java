fcfea148c4f03a054c2e96b7d00912fdae6d881b
orira22 <oriroitzaid@gmail.com>
2023-01-30 09:53:55 +0200

    A button the randomizes an image every time you click on it

--- app/src/main/java/com/example/appex042/MainActivity.java
+++ app/src/main/java/com/example/appex042/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.appex042;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView iv;
+    ImageButton imageBtn;
+    int num;
+    Random rnd = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageBtn = findViewById(R.id.imageBtn);
+        iv = findViewById(R.id.iv);
+    }
+
+    public void randomImage(View view) {
+        num = rnd.nextInt(3) + 1;
+        switch(num)
+        {
+            case 1:
+                iv.setImageResource(R.drawable.electrobunny);
+                break;
+            case 2:
+                iv.setImageResource(R.drawable.orbit);
+                break;
+            case 3:
+                iv.setImageResource(R.drawable.roboactive);
+                break;
+        }
     }
 }
